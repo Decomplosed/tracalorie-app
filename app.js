6a67dfe1c445848d51978ccbe522012a55e4c681
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-07 22:22:48 +0100

    created a functionality to add item

--- app.js
+++ app.js
@@ -17,17 +17,17 @@ const ItemCtrl = (function() {
       {
         id: 0,
         name: 'Steak Dinner',
-        calories: '1200'
+        calories: 1200
       },
       {
         id: 1,
         name: 'Cookie',
-        calories: '400'
+        calories: 400
       },
       {
         id: 2,
         name: 'Eggs',
-        calories: '300'
+        calories: 300
       },
     ],
     currentItem: null,
@@ -37,6 +37,20 @@ const ItemCtrl = (function() {
   return {
     getItems: function() {
       return data.items
+    },
+    addItem: function(name, calories) {
+      let ID
+      if(data.items.length > 0) {
+        ID = data.items[data.items.length - 1].id + 1
+      } else {
+        ID = 0
+      }
+
+      calories = parseInt(calories)
+
+      newItem = new Item(ID, name, calories)
+
+      data.items.push(newItem)
     },
     logData: function() {
       return data
@@ -48,7 +62,10 @@ const ItemCtrl = (function() {
 
 const UICtrl = (function() {
   const UISelectors = {
-    itemList: '#item-list'
+    itemList: '#item-list',
+    addBtn: '.add-btn',
+    itemNameInput: '#item-name',
+    itemCaloriesInput: '#item-calories'
   }
 
   return {
@@ -66,6 +83,15 @@ const UICtrl = (function() {
 
       // Insert list items
       document.querySelector(UISelectors.itemList).innerHTML = html
+    },
+    getItemInput: function() {
+      return {
+        name: document.querySelector(UISelectors.itemNameInput).value,
+        calories: document.querySelector(UISelectors.itemCaloriesInput).value
+      }
+    },
+    getSelectors: function() {
+      return UISelectors
     }
   }
 })()
@@ -73,12 +99,32 @@ const UICtrl = (function() {
 // App Controller
 
 const App = (function(ItemCtrl, UICtrl) {
+  // Load event listeners
+
+  const loadEventListeners = function() {
+    const UISelectors = UICtrl.getSelectors()
+
+    document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit)
+
+  }
+
+  const itemAddSubmit = function(e) {
+    const input = UICtrl.getItemInput()
+
+    if(input.name !== '' && input.calories !== '') {
+      const newItem = ItemCtrl.addItem(input.name, input.calories)
+    }
+
+    e.preventDefault()
+  }
+
 
   return {
     init: function() {
       const items = ItemCtrl.getItems()
 
       UICtrl.populateItemsList(items)
+      loadEventListeners()
     }
   }
 
